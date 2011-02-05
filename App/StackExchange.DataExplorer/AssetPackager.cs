874d3057938d0ef4102e43c158d48904dc893c02
Justin Pealing <Justin.Minnow@gmail.com>
2011-02-05 12:40:20 +1000

    Removed sorting from the results grid. (Backed out changeset: d7c3942e7cbc)

---
+++ App/StackExchange.DataExplorer/AssetPackager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using StackExchange.AssetPackager;
+
+namespace StackExchange.DataExplorer
+{
+    /// <summary>
+    /// This class handles all CSS and Javascript includes 
+    /// </summary>
+    public class AssetPackager : Packager<AssetPackager>
+    {
+        protected override bool CompressAssets
+        {
+            get
+            {
+#if DEBUG 
+                return false;
+#else
+                return true;
+#endif
+            }
+        }
+
+        protected override Dictionary<string, AssetCollection> CssAssets
+        {
+            get
+            {
+
+                return
+                new Dictionary<string, AssetCollection>
+                    {
+                        {"sitecss", new AssetCollection {"/Content/site.css"}},
+                        {
+                            "viewer_editor", new AssetCollection
+                                                 {
+                                                     "/Content/smoothness/jquery-ui-1.8.1.custom.css",
+                                                     "/Content/slickgrid/slick.grid.css",
+                                                     "/Content/codemirror/sqlcolors.css"
+                                                 }
+                            }
+                    };
+            }
+        }
+
+
+        protected override Dictionary<string, AssetCollection> JsAssets
+        {
+            get
+            {
+                return
+                new Dictionary<string, AssetCollection>
+                    {
+                        {
+                            "viewer", new AssetCollection
+                                          {
+                                              "/Scripts/codemirror/stringstream.js",
+                                              "/Scripts/codemirror/tokenize.js",
+                                              "/Scripts/codemirror/highlight.js",
+                                              "/Scripts/codemirror/parsesql.js",
+                                              "/Scripts/query.js",
+                                              "/Scripts/jquery.rule.js",
+                                              "/Scripts/jquery.event.drag-1.5.js",
+                                              "/Scripts/slick.grid.js",
+                                          }
+                            },
+                        {
+                            "editor", new AssetCollection
+                                          {
+                                              "/Scripts/jquery.rule.js",
+                                              "/Scripts/jquery.textarearesizer.js",
+                                              "/Scripts/jquery.event.drag-1.5.js",
+                                              "/Scripts/slick.grid.js",
+                                              "/Scripts/codemirror/codemirror.js",
+                                              "/Scripts/query.js"
+                                          }
+                            },
+                            
+                            { "jquery", new AssetCollection("http://ajax.microsoft.com/ajax/jquery/jquery-1.4.2.min.js")
+                                       {
+                                           "/Scripts/jquery-1.4.2.js"
+                                       }
+                            },
+ 
+                            {"jquery.validate",
+                     new AssetCollection(
+                         "http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.pack.js")
+                             {
+                                 "/Scripts/jquery.validate.js"
+                             }
+                        }
+                    
+                    };
+
+            }
+        }
+
+    }
+}
---
+++ App/StackExchange.DataExplorer/Models/DBContext.cs
@@ -0,0 +1,49 @@
+﻿using System.Transactions;
+
+namespace StackExchange.DataExplorer.Models
+{
+    /// These *MUST* be inside the Namespace! Fix for SP1 Linq to SQL Designer issue!
+    
+    partial class DBContext
+    {
+        /// <summary>
+        /// Allows scoping of query performance data for logging.
+        /// </summary>
+        public string SessionName { get; set; }
+
+        /// <summary>
+        /// Allows unit tests to easily know which site this context was created for.
+        /// </summary>
+        public Site Site { get; set; }
+
+        /// <summary>
+        /// Answers a new TransactionScope with proper options for read heavy traffic.
+        /// </summary>
+        public static TransactionScope GetTransaction()
+        {
+            return new TransactionScope(TransactionScopeOption.Required,
+                                        new TransactionOptions {IsolationLevel = IsolationLevel.ReadCommitted});
+        }
+
+        /// <summary>
+        /// Answers a new DBContext for the current site.
+        /// </summary>
+        public static DBContext GetContext()
+        {
+            return new DBContext();
+        }
+
+        /// <summary>
+        /// Ensures anonymous users aren't persisted to the database.
+        /// </summary>
+        /* public void InsertUser(User instance) {
+            if (instance == null || instance.IsAnonymous)
+                return;
+            ExecuteDynamicInsert(instance);
+        }*/
+        public override string ToString()
+        {
+            return Site != null ? Site.Name : "unknown site";
+        }
+    }
+}
