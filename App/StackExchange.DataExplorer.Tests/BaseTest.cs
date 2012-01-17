7bd50426e5dd850a202d1a58a1f6039be7889217
Tim Stone <tmslft@gmail.com>
2012-01-17 12:30:06 -0500

    Avoid replacing the revision ID with the query set ID in the form action Also remove a console.log() statement...whoops

---
+++ App/StackExchange.DataExplorer.Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StackExchange.DataExplorer.Tests {
+
+
+    [TestClass]
+    public class BaseTest {
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            Current.DB.BeginTransaction();
+        }
+
+        [TestCleanup]
+        public void Teardown() 
+        {
+            Current.DB.RollbackTransaction();
+            Current.DisposeDB();
+        }
+    }
+}
