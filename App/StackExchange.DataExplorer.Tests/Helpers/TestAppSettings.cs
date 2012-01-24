e1ecd973d48ec2534e0ef43f1727bc3ed6b31f69
Sam Saffron <sam@stackoverflow.com>
2012-01-24 14:16:28 +1100

    Backed out changeset: 89e9b5dec0ca

---
+++ App/StackExchange.DataExplorer.Tests/Helpers/TestAppSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.DataExplorer.Helpers;
+using StackExchange.DataExplorer.Models;
+
+namespace StackExchange.DataExplorer.Tests.Util {
+    [TestClass]
+    public class TestAppSettings : BaseTest
+    {
+        [TestMethod]
+        public void TestBoolSetting() {
+            Current.DB.Execute("delete from AppSettings");
+            AppSettings.Refresh();
+            Assert.IsFalse(AppSettings.EnableWhiteList);
+            Current.DB.AppSettings.Insert(new { Setting = "EnableWhiteList", Value = "true" });
+            AppSettings.Refresh();
+            Assert.IsTrue(AppSettings.EnableWhiteList);
+        }
+    }
+}
