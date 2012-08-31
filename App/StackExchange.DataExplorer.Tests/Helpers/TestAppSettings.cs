ae32d58c03c25e85a41b03545ea3dd912fe8b0ad
Tim Stone <tmslft@gmail.com>
2012-08-31 00:22:38 -0400

    If whitelisting is enabled and the user is anonymous, don't make the site selector request

---
+++ App/StackExchange.DataExplorer.Tests/Helpers/TestAppSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.DataExplorer.Helpers;
+using StackExchange.DataExplorer.Models;
+
+namespace StackExchange.DataExplorer.Tests.Util {
+    [TestClass]
+    public class TestAppSettings : BaseTest
+    {
+        [TestMethod]
+        public void TestBoolSetting() {
+            Current.DB.Execute("delete from AppSettings");
+            AppSettings.Refresh();
+            Assert.IsFalse(AppSettings.EnableWhiteList);
+            Current.DB.AppSettings.Insert(new { Setting = "EnableWhiteList", Value = "true" });
+            AppSettings.Refresh();
+            Assert.IsTrue(AppSettings.EnableWhiteList);
+        }
+
+        [TestMethod]
+        public void TestObjectSetting()
+        {
+            Current.DB.Execute("DELETE FROM AppSettings");
+            AppSettings.Refresh();
+            Assert.IsNull(AppSettings.HelperTableOptions);
+            Current.DB.AppSettings.Insert(new {
+                Setting = "HelperTableOptions",
+                Value = "{ \"PerSite\": \"true\", \"IncludePattern\": \".*Types$\" }"
+            });
+            AppSettings.Refresh();
+            Assert.IsNotNull(AppSettings.HelperTableOptions);
+            Assert.IsTrue(AppSettings.HelperTableOptions.PerSite);
+            Assert.AreEqual(".*Types$", AppSettings.HelperTableOptions.IncludePattern);
+        }
+    }
+}
---
+++ App/StackExchange.DataExplorer/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using StackExchange.DataExplorer.Models;
+using StackExchange.DataExplorer.Helpers;
+
+namespace StackExchange.DataExplorer.Controllers
+{
+    [HandleError]
+    public class HomeController : StackOverflowController
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            SetHeader("Choose a Site");
+            SelectMenuItem("Home");
+
+            var sites = Current.DB.Query<Models.Site>("select * from Sites order by TotalQuestions desc").ToList();
+
+            return View(sites);
+        }
+
+        [Route("about")]
+        public ActionResult About()
+        {
+            SetHeader("About");
+
+            return View();
+        }
+
+
+        [Route("faq")]
+        public ActionResult Faq()
+        {
+            SetHeader("Frequently Asked Questions");
+
+            return View();
+        }
+
+        [Route("sites")]
+        public ActionResult SearchSites()
+        {
+            var sites = Current.DB.Query<Site>("SELECT * FROM Sites").Select<Site, object>(
+                site =>
+                {
+                    return new
+                    {
+                        Id = site.Id,
+                        Url = site.Url,
+                        Name = site.Name,
+                        IconUrl = site.IconProxyUrl,
+                        LongName = site.LongName
+                    };
+                }
+            );
+
+            return Json(sites);
+        }
+    }
+}
